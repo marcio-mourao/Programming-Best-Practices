f31ef455064444536c8379f2adc0f164051d9592
Marcio Mourao <mdam@umich.edu>
2017-08-22 13:27:15 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 21 22:27:32 2017
+
+@author: mdam
+"""
+
+def add_to_life_universe_everything(x):
+    answer = 42
+
+    answer += x    
+    
+    return answer
+
+output=add_to_life_universe_everything(12)
+print(output)
+
